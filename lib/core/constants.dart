285ca47283319acba8e1ae9b3d7b3caad60248d4
iambinshad <binshadp999@gmail.com>
2023-03-27 19:38:59 +0530

    Initial commit

---
+++ lib/core/constants.dart
@@ -0,0 +1,9 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken = 'pk.eyJ1Ijoic21hcnRpY28iLCJhIjoiY2xmcW92amU3MDBrdDN3cngzb2JrazdkdyJ9.CWEQ56kat7wGR2qbGDWxyA';
+
+  static const String mapBoxStyleId = 'https://api.mapbox.com/styles/v1/smartico/clfqpjbhx000k01pleq9oc5av/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21hcnRpY28iLCJhIjoiY2xmcW92amU3MDBrdDN3cngzb2JrazdkdyJ9.CWEQ56kat7wGR2qbGDWxyA';
+
+  static final myLocation = LatLng(51.5090214, -0.1982948);
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mapbox/core/constants.dart';
+
+import 'model.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+var currentLocation = AppConstants.myLocation;
+
+class _HomePageState extends State<HomePage> {
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 33, 32, 32),
+        title: const Text('Flutter MapBox'),
+      ),
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              minZoom: 5,
+              maxZoom: 18,
+              zoom: 13,
+              center: currentLocation,
+            ),
+            layers: [
+              TileLayerOptions(
+                urlTemplate:
+                    "https://api.mapbox.com/styles/v1/smartico/clfqpjbhx000k01pleq9oc5av/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21hcnRpY28iLCJhIjoiY2xmcW92amU3MDBrdDN3cngzb2JrazdkdyJ9.CWEQ56kat7wGR2qbGDWxyA",
+                additionalOptions: {
+                  'mapStyleId': AppConstants.mapBoxStyleId,
+                  'accessToken': AppConstants.mapBoxAccessToken,
+                },
+              ),
+              MarkerLayerOptions(
+                markers: [
+                  for (int i = 0; i < mapMarkers.length; i++)
+                    Marker(
+                      height: 40,
+                      width: 40,
+                      point: mapMarkers[i].location ?? AppConstants.myLocation,
+                      builder: (_) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: const Icon(Icons.place,color: Colors.red,),
+                        );
+                      },
+                    ),
+                ]
+              )
+            ],
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 2,
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: PageView.builder(
+              onPageChanged: (value) {},
+              itemCount: mapMarkers.length,
+              itemBuilder: (_, index) {
+                final item = mapMarkers[index];
+                return Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    color: const Color.fromARGB(255, 30, 29, 29),
+                    child: Row(
+                      children: [
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: ListView.builder(
+                                  padding: EdgeInsets.zero,
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: item.rating,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return const Icon(
+                                      Icons.star,
+                                      color: Colors.orange,
+                                    );
+                                  },
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      item.title ?? '',
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      item.address ?? '',
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset(
+                                item.image ?? '',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+
+          
+        ],
+        
+      ),
+    );
+  }
+}
